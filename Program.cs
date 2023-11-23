3e1322986c67a44b1c6dc5540fe28bd25a9cdd75
Michal Sifalda <spitzlam09@gmail.com>
2023-11-23 09:57:40 +0100

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool alfa = true;
+string result = "";
+while (alfa)
+{
+    Console.WriteLine("Zadejte slovo");
+    var input2 = Console.ReadLine();
+    
+    if (string.IsNullOrWhiteSpace(input2))
+    {
+        alfa = false;
+    }
+    result += " ";
+    result += input2;
+}
+Console.WriteLine(result);
